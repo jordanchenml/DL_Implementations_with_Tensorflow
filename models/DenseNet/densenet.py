981fde94d7e619abadf1cb887270053bc0fa07ec
Jordan Chen <jordanchenml@gmail.com>
2020-01-22 13:33:51 +0800

    Add Dense Net.

--- models/DenseNet/densenet.py
+++ models/DenseNet/densenet.py
@@ -1,5 +1,13 @@
 import tensorflow as tf
-from configuration import NUM_CLASSES
+tf.config.experimental_run_functions_eagerly(True)
+print(tf.__version__)
+
+# Parameters
+ACCURACY_THRESHOLD = 0.99
+NUM_CLASSES = 10
+IMAGE_HEIGHT = 32
+IMAGE_WIDTH = 32
+CHANNELS = 3
 
 
 class BottleNeck(tf.keras.layers.Layer):
@@ -130,6 +138,17 @@ class DenseNet(tf.keras.Model):
 
         return x
 
+    def build_graph(self, input_shape):
+        input_shape_nobatch = input_shape[1:]
+        self.build(input_shape)
+        inputs = tf.keras.Input(shape=input_shape_nobatch)
+
+        if not hasattr(self, 'call'):
+            raise AttributeError(
+                "User should define 'call' method in sub-class model!")
+
+        _ = self.call(inputs)
+
 
 def densenet_121():
     return DenseNet(num_init_features=64, growth_rate=32,
@@ -153,3 +172,87 @@ def densenet_264():
     return DenseNet(num_init_features=64, growth_rate=32,
                     block_layers=[6, 12, 64, 48], compression_rate=0.5,
                     drop_rate=0.5)
+
+
+def prepare_dataset():
+    cifar10 = tf.keras.datasets.cifar10
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+
+    x_train = tf.convert_to_tensor(x_train)
+    x_test = tf.convert_to_tensor(x_test)
+
+    y_train = tf.convert_to_tensor(y_train)
+    y_test = tf.convert_to_tensor(y_test)
+
+    print("### Dataset Shape ###")
+    print('x_train shape:', x_train.shape)
+    print('x_test shape:', x_test.shape)
+    print('y_train shape:', y_train.shape)
+    print('y_test shape:', y_test.shape)
+
+    return x_train, x_test, y_train, y_test
+
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('accuracy'):
+            if (logs.get('accuracy') > 0.99):
+                print("\nReached 99% accuracy so cancelling training!")
+                self.model.stop_training = True
+
+
+def print_model_summary(network):
+    network.build_graph((1, IMAGE_HEIGHT, IMAGE_WIDTH, CHANNELS))
+    network.summary()
+
+
+
+if __name__ == '__main__':
+    # GPU settings
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        print("### Using GPU ###")
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+
+    x_train, x_test, y_train, y_test = prepare_dataset()
+
+    model = densenet_121()
+    print_model_summary(model)
+
+    initial_learning_rate = 0.1
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate,
+        decay_steps=100000,
+        decay_rate=0.96,
+        staircase=True)
+
+    model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr_schedule,
+                                                    momentum=0.0,
+                                                    nesterov=False),
+                    loss='sparse_categorical_crossentropy',
+                    metrics=['accuracy'])
+
+    print('\n### Fit model on training data ###')
+    callbacks = myCallback()
+    history = model.fit(x_train, y_train, epochs=1500, validation_split=0.1, batch_size=1024, callbacks=[callbacks])
+    print('\nhistory dict:', history.history)
+
+    model.evaluate(x_test, y_test, verbose=2)
+
+    print('\nhistory dict:', history.history)
+
+    # Evaluate the model on the test data using `evaluate`
+    print('\n# Evaluate on test data')
+    results = model.evaluate(x_test, y_test, batch_size=128)
+    print('test loss, test acc:', results)
+
+    # Generate predictions (probabilities -- the output of the last layer)
+    # on new data using `predict`
+    print('\n# Generate predictions for 3 samples')
+    predictions = model.predict(x_test[:3])
+    print('predictions:', predictions.argmax(axis=1))
+    print('Answer:', y_test[:3])
